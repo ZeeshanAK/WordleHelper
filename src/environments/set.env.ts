663a3843702ff3fd7d849996b1d0d3a242fe00da
ZeeshanAK <zeeshankhan.a1@gmail.com>
2022-01-27 12:45:35 +0500

    idk what am doing

--- src/environments/set.env.ts
+++ src/environments/set.env.ts
@@ -1,30 +1,29 @@
 const setEnv = () => {
-    const fs = require('fs');
-    const writeFile = fs.writeFile;
-  // Configure Angular `environment.ts` file path
-    const targetPath = './src/environments/environment.ts';
-  // Load node modules
-    const colors = require('colors');
-    const appVersion = require('../../package.json').version;
-    require('dotenv').config({
-      path: 'src/environments/.env'
-    });
-  // `environment.ts` file structure
-    const envConfigFile = `export const environment = {
+  const fs = require('fs');
+  const writeFile = fs.writeFile;
+// Configure Angular `environment.ts` file path
+  const targetPath = './src/environments/environment.ts';
+// Load node modules
+  const colors = require('colors');
+  const appVersion = require('../../package.json').version;
+  require('dotenv').config({
+    path: 'src/environments/.env'
+  });
+// `environment.ts` file structure
+  const envConfigFile = `export const environment = {
     ping: '${process.env.ping}',
-    production: true,
-  };
-  `;
-    console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
-    console.log(colors.grey(envConfigFile));
-    writeFile(targetPath, envConfigFile, (err: any) => {
-      if (err) {
-        console.error(err);
-        throw err;
-      } else {
-        console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
-      }
-    });
-  };
-  
-  setEnv();
+  production: true,
+};
+`;
+  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+  writeFile(targetPath, envConfigFile, (err: any) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+    }
+  });
+};
+
+setEnv();
